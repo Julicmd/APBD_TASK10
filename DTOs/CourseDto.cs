695cded250f42e92a3eac7dc51d951954ea59992
Julius Babelis <s31850@pjwst.edu.pl>
2026-06-08 21:58:32 +0200

    DTOs

--- DTOs/CourseDto.cs
+++ DTOs/CourseDto.cs
@@ -2,5 +2,7 @@ namespace StudentPanel.DTOs;
 
 public class CourseDto
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public int Ects { get; set; }
 }
--- DTOs/StudentCourseDto.cs
+++ DTOs/StudentCourseDto.cs
@@ -2,5 +2,7 @@ namespace StudentPanel.DTOs;
 
 public class StudentCourseDto
 {
-    
+    public int StudentId { get; set; }
+    public int CourseId { get; set; }
+    public DateTime AssignedAt { get; set; }
 }
